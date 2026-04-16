61a094e1b587f7c04b4fd036fb58a59eccbb488d
mukul dhiman <55916608+Mukul-dhiman@users.noreply.github.com>
2026-04-16 14:35:34 +0530

    Add: combination-target-sum-ii - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-4.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    void comb(vector<int>& nums, int target, int idx, vector<vector<int>>& res, vector<int>& curr, int& curr_sum){
+        if(curr_sum == target){
+            res.push_back(curr);
+            return;
+        }
+
+        if(curr_sum>target){
+            return;
+        }
+
+        for(int i=idx;i<nums.size();i++){
+            if(i>idx && nums[i]==nums[i-1]){
+                continue;
+            }
+            if(curr_sum+nums[i]>target){
+                break;
+            }
+            curr_sum+=nums[i];
+            curr.push_back(nums[i]);
+            comb(nums,target, i+1, res, curr, curr_sum);
+            curr.pop_back();
+            curr_sum-=nums[i];
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        vector<int> curr;
+        // set<vector<int>> res;
+        vector<vector<int>> res;
+        int curr_sum = 0;
+        sort(nums.begin(), nums.end());
+        comb(nums, target, 0, res, curr, curr_sum);
+        // for(auto v:res){ret.push_back(v);}
+        return res;
+    }
+};
